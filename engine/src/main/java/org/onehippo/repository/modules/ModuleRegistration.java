322ad37a1266d12989bf6af3782ba6801b9a7bc8
Unico Hommes <u.hommes@onehippo.com>
2013-03-04 11:23:31 +0000

    REPO-624 backport repository-configurable daemon modules and inter-module dependency management

---
+++ engine/src/main/java/org/onehippo/repository/modules/ModuleRegistration.java
@@ -0,0 +1,119 @@
+/*
+ *  Copyright 2013 Hippo B.V. (http://www.onehippo.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.onehippo.repository.modules;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+class ModuleRegistration {
+
+    private final String moduleName;
+    private final Class<? extends DaemonModule> moduleClass;
+    private DaemonModule module;
+
+    ModuleRegistration(String moduleName, Class<? extends DaemonModule> moduleClass) {
+        this.moduleName = moduleName;
+        this.moduleClass = moduleClass;
+    }
+
+    ModuleRegistration(DaemonModule module) {
+        this(module.getClass().getName(), module.getClass());
+        this.module = module;
+    }
+
+    String getModuleName() {
+        return moduleName;
+    }
+
+    Class<? extends DaemonModule> getModuleClass() {
+        return moduleClass;
+    }
+
+    DaemonModule getModule() throws IllegalAccessException, InstantiationException {
+        if (module == null) {
+            module = moduleClass.newInstance();
+        }
+        return module;
+    }
+
+    Collection<Class<?>> provides() {
+        final ProvidesService annotation = moduleClass.getAnnotation(ProvidesService.class);
+        if (annotation != null) {
+            return Arrays.asList(annotation.types());
+        }
+        return Collections.emptyList();
+    }
+
+    Collection<Class<?>> requirements() {
+        final RequiresService annotation = moduleClass.getAnnotation(RequiresService.class);
+        if (annotation != null) {
+            return Arrays.asList(annotation.types());
+        }
+        return Collections.emptyList();
+    }
+
+    boolean requires(ModuleRegistration other) {
+        final Collection<Class<?>> requirements = requirements();
+        if (!requirements.isEmpty()) {
+            final Collection<Class<?>> provides = other.provides();
+            for (Class<?> requirement : requirements) {
+                if (provides.contains(requirement)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    int compare(ModuleRegistration other, List<ModuleRegistration> all) {
+        if (this.requires(other)) {
+            return 1;
+        }
+        if (other.requires(this)) {
+            return -1;
+        }
+        final List<ModuleRegistration> dependencies = new ArrayList<ModuleRegistration>();
+        final List<ModuleRegistration> dependents = new ArrayList<ModuleRegistration>();
+        for (ModuleRegistration registration : all) {
+            if (registration == this || registration == other) {
+                continue;
+            }
+            if (this.requires(registration)) {
+                dependencies.add(registration);
+            }
+            if (registration.requires(this)) {
+                dependents.add(registration);
+            }
+        }
+        final ArrayList<ModuleRegistration> rest = new ArrayList<ModuleRegistration>(all);
+        rest.remove(this);
+        for (ModuleRegistration dependency : dependencies) {
+            if (dependency.compare(other, rest) > 0) {
+                return 1;
+            }
+        }
+        for (ModuleRegistration dependent : dependents) {
+            if (dependent.compare(other, rest) < 0) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+}
---
+++ engine/src/main/java/org/onehippo/repository/modules/ModuleRegistry.java
@@ -0,0 +1,118 @@
+/*
+ *  Copyright 2013 Hippo B.V. (http://www.onehippo.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.onehippo.repository.modules;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.jcr.RepositoryException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class ModuleRegistry {
+
+    private static final Logger log = LoggerFactory.getLogger(ModuleRegistry.class);
+
+    private List<ModuleRegistration> registrations = new ArrayList<ModuleRegistration>();
+
+    ModuleRegistry() {}
+
+    void registerModule(DaemonModule module) throws RepositoryException {
+        addRegistration(new ModuleRegistration(module));
+    }
+
+    void registerModule(String moduleName, Class<? extends DaemonModule> moduleClass) throws RepositoryException {
+        addRegistration(new ModuleRegistration(moduleName, moduleClass));
+    }
+
+    private void addRegistration(ModuleRegistration registration) throws RepositoryException {
+        final List<ModuleRegistration> updated = new ArrayList<ModuleRegistration>(registrations);
+        updated.add(registration);
+        checkDependencyGraph(updated);
+        sortModules(updated);
+        registrations = updated;
+    }
+
+    private void sortModules(final List<ModuleRegistration> registrations) throws RepositoryException {
+        Collections.sort(registrations, new Comparator<ModuleRegistration>() {
+            @Override
+            public int compare(final ModuleRegistration o1, final ModuleRegistration o2) {
+                return o1.compare(o2, registrations);
+            }
+        });
+    }
+
+    List<ModuleRegistration> getModuleRegistrations() {
+        return Collections.unmodifiableList(registrations);
+    }
+
+    List<ModuleRegistration> getModuleRegistrationsReverseOrder() {
+        final ArrayList<ModuleRegistration> moduleRegistrations = new ArrayList<ModuleRegistration>(registrations);
+        Collections.reverse(moduleRegistrations);
+        return Collections.unmodifiableList(moduleRegistrations);
+    }
+
+    void checkDependencyGraph(List<ModuleRegistration> registrations) throws RepositoryException {
+        final Map<Class<?>, ModuleRegistration> provided = new HashMap<Class<?>, ModuleRegistration>();
+        for (ModuleRegistration registration : registrations) {
+            for (Class<?> aClass : registration.provides()) {
+                provided.put(aClass, registration);
+            }
+        }
+        final Map<ModuleRegistration, List<ModuleRegistration>> requirements =
+                new HashMap<ModuleRegistration, List<ModuleRegistration>>();
+        for (ModuleRegistration registration : registrations) {
+            final List<ModuleRegistration> requires = new ArrayList<ModuleRegistration>();
+            for (Class<?> requiredClass : registration.requirements()) {
+                if (provided.containsKey(requiredClass)) {
+                    requires.add(provided.get(requiredClass));
+                } else {
+                    log.warn("Module {} has unsatisfied dependency on service {}",
+                            registration.getModuleName(), requiredClass);
+                }
+            }
+            requirements.put(registration, requires);
+        }
+        for (ModuleRegistration registration : registrations) {
+            detectCircularDependency(registration, requirements, new HashSet<ModuleRegistration>());
+        }
+    }
+
+    private void detectCircularDependency(final ModuleRegistration registration,
+                                          final Map<ModuleRegistration, List<ModuleRegistration>> requirements,
+                                          final Set<ModuleRegistration> registrations) throws RepositoryException {
+        if (registrations.contains(registration)) {
+            final StringBuilder sb = new StringBuilder();
+            for (ModuleRegistration m : registrations) {
+                sb.append(m.getModuleName() != null ? m.getModuleName() : m.getModuleClass());
+                sb.append(", ");
+            }
+            sb.append(registration.getModuleName() != null ? registration.getModuleName() : registration.getModuleClass());
+            throw new RepositoryException("Circular dependency detected among modules: " + sb);
+        }
+        registrations.add(registration);
+        for (ModuleRegistration requirement : requirements.get(registration)) {
+            detectCircularDependency(requirement, requirements, new HashSet<ModuleRegistration>(registrations));
+        }
+    }
+}
