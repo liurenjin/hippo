5f51194cd512faf0e39d4429c326f2da9cd6a77b
Unico Hommes <u.hommes@onehippo.com>
2013-09-09 13:48:10 +0000

    REPO-744 backport: override checking for unlock privileges to allow users with lock management rights to unlock locks: the default jackrabbit behavior only allows the session that owns the lock to unlock the node

--- engine/src/main/java/org/hippoecm/repository/jackrabbit/HippoLockManager.java
+++ engine/src/main/java/org/hippoecm/repository/jackrabbit/HippoLockManager.java
@@ -19,10 +19,15 @@ import java.util.concurrent.ScheduledExecutorService;
 
 import javax.jcr.ItemNotFoundException;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.lock.LockException;
 
 import org.apache.jackrabbit.core.fs.FileSystem;
 import org.apache.jackrabbit.core.id.NodeId;
+import org.apache.jackrabbit.core.lock.LockInfo;
 import org.apache.jackrabbit.core.lock.LockManagerImpl;
+import org.apache.jackrabbit.core.security.authorization.Permission;
+import org.apache.jackrabbit.spi.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,6 +46,18 @@ public class HippoLockManager extends LockManagerImpl {
      */
     public HippoLockManager(org.apache.jackrabbit.core.SessionImpl session, FileSystem fs, ScheduledExecutorService executor) throws RepositoryException {
         super(session, fs, executor);
+    }
+
+    @Override
+    protected void checkUnlock(final LockInfo info, final Session session) throws LockException, RepositoryException {
+        if (session instanceof org.apache.jackrabbit.core.SessionImpl) {
+            final org.apache.jackrabbit.core.SessionImpl sessionImpl = ((org.apache.jackrabbit.core.SessionImpl) session);
+            final Path path = sessionImpl.getHierarchyManager().getPath(info.getId());
+            if (sessionImpl.getAccessManager().isGranted(path, Permission.LOCK_MNGMT)) {
+                return;
+            }
+        }
+        super.checkUnlock(info, session);
     }
 
     @Override
