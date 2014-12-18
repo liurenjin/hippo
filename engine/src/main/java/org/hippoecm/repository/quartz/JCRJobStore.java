ecff8b4de7c2846e7f548f161eb1bfe363807486
Unico Hommes <u.hommes@onehippo.com>
2014-12-18 13:45:02 +0000

    REPO-1164 JCRJobStore might return wrong number of triggers from #acquireNextTriggers

--- engine/src/main/java/org/hippoecm/repository/quartz/JCRJobStore.java
+++ engine/src/main/java/org/hippoecm/repository/quartz/JCRJobStore.java
@@ -583,9 +583,6 @@ public class JCRJobStore implements JobStore {
         synchronized (session) {
             try {
                 for (Node triggerNode : getPendingTriggers(session, noLaterThan)) {
-                    if (maxCount-- <= 0) {
-                        break;
-                    }
                     if (!JcrUtils.getBooleanProperty(triggerNode, HIPPOSCHED_ENABLED, true)) {
                         continue;
                     }
@@ -602,6 +599,9 @@ public class JCRJobStore implements JobStore {
                                     triggers = new ArrayList<>();
                                 }
                                 triggers.add(createTriggerFromNode(triggerNode));
+                                if (--maxCount <= 0) {
+                                    break;
+                                }
                             } else {
                                 unlock(session, triggerNode.getPath());
                             }
