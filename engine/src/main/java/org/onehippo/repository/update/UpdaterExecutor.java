0455729101dce94a9427cfdf5a539d0964a02c3d
Unico Hommes <u.hommes@onehippo.com>
2015-01-05 15:22:14 +0000

    REPO-1174 Allow anonymous login in PingServlet and make it the default

--- engine/src/main/java/org/onehippo/repository/update/UpdaterExecutor.java
+++ engine/src/main/java/org/onehippo/repository/update/UpdaterExecutor.java
@@ -137,7 +137,6 @@ public class UpdaterExecutor implements EventListener {
         } catch (RepositoryException e) {
             error("Failed to log cancel event for " + updaterInfo.getName(), e);
         }
-        report.finish();
         cancelled = true;
     }
 
