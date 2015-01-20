0f3feea1cc4919a2fc05e6bfac44beeb4e1bac1a
Bert Leunis <b.leunis@onehippo.com>
2015-01-20 13:24:34 +0000

    CMS7-8650 prevent creation of same name siblings for gallery and assets items

--- builtin/src/main/java/org/hippoecm/repository/gallery/GalleryWorkflow.java
+++ builtin/src/main/java/org/hippoecm/repository/gallery/GalleryWorkflow.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
+ *  Copyright 2008-2015 Hippo B.V. (http://www.onehippo.com)
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -22,10 +22,11 @@ import javax.jcr.RepositoryException;
 
 import org.hippoecm.repository.api.Workflow;
 import org.hippoecm.repository.api.Document;
+import org.hippoecm.repository.api.WorkflowException;
 
 public interface GalleryWorkflow extends Workflow
 {
 
     public List<String> getGalleryTypes() throws RemoteException, RepositoryException;
-    public Document createGalleryItem(String name, String type) throws RemoteException, RepositoryException;
+    public Document createGalleryItem(String name, String type) throws RemoteException, RepositoryException, WorkflowException;
 }
