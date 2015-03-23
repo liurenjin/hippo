3f3c959e9670acdc995c3cb798ca0c3324a446a4
Unico Hommes <u.hommes@onehippo.com>
2015-03-23 12:39:06 +0000

    REPO-1198 In the case that the job runs just before the event is logged, it will take (at least) 5 seconds before the next run occurs. This leaves a 100ms window for the broadcast thread to wake up and run the job that should notify the listener. Such a window may be too short on a heavily loaded system.

--- modules/src/test/java/org/hippoecm/repository/events/BroadcastModuleTest.java
+++ modules/src/test/java/org/hippoecm/repository/events/BroadcastModuleTest.java
@@ -134,7 +134,7 @@ public class BroadcastModuleTest extends RepositoryTestCase {
     }
 
     private void waitForEvent(final Listener listener) throws Exception {
-        int n = 51;
+        int n = 101;
         while (n-- > 0) {
             Thread.sleep(100);
             if (listener.seenEvents.size() == 1) {
@@ -142,6 +142,6 @@ public class BroadcastModuleTest extends RepositoryTestCase {
                 return;
             }
         }
-        throw new Exception("Event not received within 5 seconds");
+        throw new Exception("Event not received within 10 seconds");
     }
 }
