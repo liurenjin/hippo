6cba96b83f6371b06ceff290420c302d3de7ed3b
Unico Hommes <u.hommes@onehippo.com>
2015-04-18 09:16:43 +0000

    REPO-1227 backward compatible constructor

--- api/src/main/java/org/hippoecm/repository/util/NodeInfo.java
+++ api/src/main/java/org/hippoecm/repository/util/NodeInfo.java
@@ -17,28 +17,69 @@ package org.hippoecm.repository.util;
 
 import java.util.Arrays;
 
+import javax.jcr.LoginException;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import javax.jcr.nodetype.ConstraintViolationException;
 import javax.jcr.nodetype.NodeDefinition;
 import javax.jcr.nodetype.NodeType;
 import javax.jcr.nodetype.NodeTypeManager;
 
+import org.onehippo.cms7.services.HippoServiceRegistry;
+import org.onehippo.repository.RepositoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class NodeInfo {
+
+    private static NodeTypeManager nodeTypeManager;
 
     private final String name;
 
     private final NodeType nodeType;
-
+    private final String nodeTypeName;
     private final NodeType[] mixinTypes;
-
+    private final String[] mixinTypeNames;
     private final int index;
+
+    /**
+     * @deprecated since 2.26.13, use {@link #NodeInfo(String, int, javax.jcr.nodetype.NodeType, javax.jcr.nodetype.NodeType[])}
+     */
+    @Deprecated
+    public NodeInfo(String name, int index, String nodeTypeName, String[] mixinTypeNames) {
+        final Logger log = LoggerFactory.getLogger(NodeInfo.class);
+        final String message = "This constructor is deprecated and very inefficient, please use alternative constructor";
+        if (log.isDebugEnabled()) {
+            log.warn(message, new Exception());
+        } else {
+            log.warn(message + ". Stack trace on debug level");
+        }
+        this.name = name;
+        this.index = index;
+        this.nodeTypeName = nodeTypeName;
+        this.mixinTypeNames = mixinTypeNames;
+        try {
+            this.nodeType = getNodeTypeManager().getNodeType(nodeTypeName);
+            this.mixinTypes = new NodeType[mixinTypeNames.length];
+            for (int i = 0; i < mixinTypeNames.length; i++) {
+                mixinTypes[i] = getNodeTypeManager().getNodeType(mixinTypeNames[i]);
+            }
+        } catch (RepositoryException e) {
+            throw new IllegalStateException("Can't initialize types", e);
+        }
+    }
 
     public NodeInfo(String name, int index, NodeType nodeType, NodeType[] mixinTypes) {
         this.name = name;
         this.nodeType = nodeType;
+        this.nodeTypeName = nodeType.getName();
         this.index = index;
         this.mixinTypes = mixinTypes;
+        this.mixinTypeNames = new String[mixinTypes.length];
+        for (int i = 0; i < mixinTypes.length; i++) {
+            this.mixinTypeNames[i] = mixinTypes[i].getName();
+        }
     }
 
     public NodeInfo(Node child) throws RepositoryException {
@@ -56,7 +97,7 @@ public final class NodeInfo {
     public NodeType getNodeType() { return nodeType; }
 
     public String getNodeTypeName() {
-        return nodeType.getName();
+        return nodeTypeName;
     }
 
     public NodeType[] getMixinTypes() {
@@ -64,11 +105,7 @@ public final class NodeInfo {
     }
 
     public String[] getMixinNames() {
-        final String[] mixinNames = new String[mixinTypes.length];
-        for (int i = 0; i < mixinTypes.length; i++) {
-            mixinNames[i] = mixinTypes[i].getName();
-        }
-        return mixinNames;
+        return mixinTypeNames;
     }
 
     public NodeDefinition getApplicableChildNodeDef(NodeType[] parentTypes) throws RepositoryException {
@@ -106,5 +143,13 @@ public final class NodeInfo {
     @Override
     public String toString() {
         return "NodeInfo[" + getName() + '[' + getIndex() + "](type=" + getNodeTypeName() + ", mixins=" + Arrays.toString(getMixinNames()) +")]";
+    }
+
+    private static NodeTypeManager getNodeTypeManager() throws RepositoryException {
+        if (nodeTypeManager == null) {
+            final RepositoryService repository = HippoServiceRegistry.getService(RepositoryService.class);
+            nodeTypeManager = repository.login().getWorkspace().getNodeTypeManager();
+        }
+        return nodeTypeManager;
     }
 }
